f58edce8b945212b27bea280abeb35b59305955c
shadeemerhi <smerhi@paystone.com>
2022-06-03 16:37:16 -0600

    install and configure graphql-ws

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,18 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import {
-  ApolloClient,
-  InMemoryCache,
-  ApolloProvider,
-  useQuery,
-  gql,
-} from "@apollo/client";
-
-const client = new ApolloClient({
-  uri: "http://localhost:4000/graphql",
-  cache: new InMemoryCache(),
-});
+import { ApolloProvider } from "@apollo/client";
+import { client } from "./graphql/apolloClient";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
